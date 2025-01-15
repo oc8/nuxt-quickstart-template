7887aa73834c93b47b3c9fff2251d62ec274af61
ocean - 11 <46847941+oc8@users.noreply.github.com>
2025-01-15 11:35:36 -0800

    Fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const animate = require("tailwindcss-animate")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
+  important: true,
   content: [
     './pages/**/*.{ts,tsx,vue}',
     './components/**/*.{ts,tsx,vue}',
